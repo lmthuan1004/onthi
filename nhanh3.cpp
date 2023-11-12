6493ee1b0b344f0c8486db456ffe912ddbe7f2e9
Thuan <mthuan1004@gmail.com>
2023-11-12 23:18:03 +0700

    nhanh3

---
+++ nhanh3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "LE MINH THUAN";
+    cout << "asdasdasdasd";
+    cout << "fgdfg";
+    cout << "assdfswerf3sdasd";
+    cout << "fgdfg";
+    cout << "assdfswerf3sdasd";
+    return 0;
+}
